5c8e897acd87575419cd38f0e6ff44e00ab94904
investingsnippets <investingsnippets@gmail.com>
2021-09-05 10:59:37 +0200

    fixes tox and adds docs

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. Investingsnippets documentation master file, created by
+   sphinx-quickstart on Sun Sep  5 10:51:32 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Investingsnippets's documentation!
+=============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- pyinvestingsnippets/drawdown.py
+++ pyinvestingsnippets/drawdown.py
@@ -1,8 +1,29 @@
-
 from pandas import DataFrame
+
 
 class Drawdown:
     """Given a wealth index DataFrame, will produce usefull drawdown metrics"""
 
     def __init__(self, df: DataFrame):
         self.wealth_index_df = df
+
+    def get_drawdown(self):
+        """
+        Summary line.
+
+        Extended description of function.
+
+        Parameters
+        ----------
+        arg1 : int
+            Description of arg1
+        arg2 : str
+            Description of arg2
+
+        Returns
+        -------
+        int
+            Description of return value
+
+        """
+        return None
