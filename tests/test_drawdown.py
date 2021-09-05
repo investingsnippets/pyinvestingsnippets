ed5ec91bb27833ba9a7b485b070e80195b6822b2
investingsnippets <investingsnippets@gmail.com>
2021-09-05 17:42:08 +0200

    Transforms everything to pandas extensions

--- tests/test_drawdown.py
+++ tests/test_drawdown.py
@@ -1,36 +1,38 @@
 import pandas as pd
 import numpy as np
 from pyinvestingsnippets import Drawdown
+from datetime import timedelta
 
 
 def test_drawdown_and_returns_series():
-    wealth_index = pd.Series(data=[0.4, 0.3, 0.2, 0.5], index=[0,1,2,3])
-    dd = Drawdown(wealth_index)
+    index_range = pd.date_range(start=pd.datetime(2000, 1, 1), periods=4, freq='AS-JAN')
+    wealth_index = pd.Series(data=[0.4, 0.3, 0.2, 0.5], index=index_range)
+    dd = wealth_index.Drawdown
     assert dd is not None
-    drawdown_df = dd.get_drawdown()
+    drawdown_df = dd.drawdown
     assert drawdown_df is not None
     assert isinstance(drawdown_df, pd.Series)
     assert drawdown_df.dtypes == 'float64'
     assert drawdown_df.name == 'Drawdown'
-    np.testing.assert_almost_equal(drawdown_df[0], 0.0)
-    np.testing.assert_almost_equal(drawdown_df[1], -0.25)
-    np.testing.assert_almost_equal(drawdown_df[2], -0.5)
-    np.testing.assert_almost_equal(drawdown_df[3], 0.0)
+    np.testing.assert_almost_equal(drawdown_df['2000-01-01'], 0.0)
+    np.testing.assert_almost_equal(drawdown_df['2001-01-01'], -0.25)
+    np.testing.assert_almost_equal(drawdown_df['2002-01-01'], -0.5)
+    np.testing.assert_almost_equal(drawdown_df['2003-01-01'], 0.0)
 
 
 def test_max_drawdown():
-    wealth_index = pd.Series(data=[0.4, 0.3, 0.2, 0.5], index=[0,1,2,3])
-    dd = Drawdown(wealth_index)
-    assert dd.get_max_drawdown() == -0.5
+    index_range = pd.date_range(start=pd.datetime(2000, 1, 1), periods=4, freq='AS-JAN')
+    wealth_index = pd.Series(data=[0.4, 0.3, 0.2, 0.5], index=index_range)
+    assert wealth_index.Drawdown.max_drawdown == -0.5
 
 
 def test_durations():
-    wealth_index = pd.Series(data=[0.4, 0.3, 0.2, 0.5, 0.4, 0.4, 0.3, 0.3, 0.5], index=[0,1,2,3,4,5,6,7,8]) 
-    dd = Drawdown(wealth_index)
-    durations = dd.compute_drawdown_lagoons_durations()
+    index_range = pd.date_range(start=pd.datetime(2000, 1, 1), periods=9, freq='AS-JAN')
+    wealth_index = pd.Series(data=[0.4, 0.3, 0.2, 0.5, 0.4, 0.4, 0.3, 0.3, 0.5], index=index_range) 
+    durations = wealth_index.Drawdown.durations
     assert isinstance(durations, pd.Series)
-    assert durations.dtypes == 'float64'
+    assert durations.dtypes == 'timedelta64[ns]'
     assert durations.name == 'Durations'
     assert len(durations) == 2
-    assert durations[3] == 3
-    assert durations[8] == 5
+    assert durations['2003-01-01'] == timedelta(days=1096)
+    assert durations['2008-01-01'] == timedelta(days=1826)
--- tests/test_waelthindex.py
+++ tests/test_waelthindex.py
@@ -6,32 +6,24 @@ from pyinvestingsnippets import WealthIndex
 def test_wealth_index():
     index_range = pd.date_range(start=pd.datetime(2000, 1, 1), periods=5, freq='AS-JAN')
     returns = pd.Series(data=[np.nan, 0.4, 0.3, 0.2, 0.5], index=index_range)
-    wi = WealthIndex(returns)
-    assert wi is not None
-    wi_index = wi.get_wealth_index()
-    assert wi_index is not None
-    assert isinstance(wi_index, pd.Series)
-    assert wi_index.dtypes == 'float64'
-    assert wi_index.name == 'WealthIndex'
-    print(wi_index)
-    assert pd.isnull(wi_index['2000-01-01'])
-    np.testing.assert_almost_equal(wi_index['2001-01-01'], 1.4)
-    np.testing.assert_almost_equal(wi_index['2002-01-01'], 1.82)
-    np.testing.assert_almost_equal(wi_index['2003-01-01'], 2.184)
-    np.testing.assert_almost_equal(wi_index['2004-01-01'], 3.276)
+    assert returns.WealthIndex is not None
+    assert isinstance(returns.WealthIndex.wealth_index, pd.Series)
+    assert returns.WealthIndex.wealth_index.dtypes == 'float64'
+    assert returns.WealthIndex.wealth_index.name == 'WealthIndex'
+    assert pd.isnull(returns.WealthIndex.wealth_index['2000-01-01'])
+    np.testing.assert_almost_equal(returns.WealthIndex.wealth_index['2001-01-01'], 1.4)
+    np.testing.assert_almost_equal(returns.WealthIndex.wealth_index['2002-01-01'], 1.82)
+    np.testing.assert_almost_equal(returns.WealthIndex.wealth_index['2003-01-01'], 2.184)
+    np.testing.assert_almost_equal(returns.WealthIndex.wealth_index['2004-01-01'], 3.276)
 
 
 def test_get_total_return():
     index_range = pd.date_range(start=pd.datetime(2000, 1, 1), periods=5, freq='AS-JAN')
     returns = pd.Series(data=[np.nan, 0.4, 0.3, 0.2, 0.5], index=index_range)
-    wi = WealthIndex(returns)
-    wi_total_ret = wi.get_total_return()
-    np.testing.assert_almost_equal(wi_total_ret, 3.276)
+    np.testing.assert_almost_equal(returns.WealthIndex.total_return, 3.276)
 
 def test_get_compound_annual_growth_rate():
     index_range = pd.date_range(start=pd.datetime(2000, 1, 1), periods=5, freq='AS-JAN')
     returns = pd.Series(data=[np.nan, 0.4, 0.3, 0.2, 0.5], index=index_range)
-    wi = WealthIndex(returns)
-    wi_total_ret = wi.get_compound_annual_growth_rate()
-    np.testing.assert_almost_equal(wi_total_ret, 0.2678526)
+    np.testing.assert_almost_equal(returns.WealthIndex.cagr, 0.2678526)
 
