27d5eca7c442a1c5512844c694797e6c49d25d98
investingsnippets <investingsnippets@gmail.com>
2021-09-05 17:42:08 +0200

    Transforms everything to pandas extensions

--- pyinvestingsnippets/drawdown.py
+++ pyinvestingsnippets/drawdown.py
@@ -5,16 +5,23 @@ import matplotlib.ticker as mtick
 import matplotlib.dates as mdates
 
 
+@pd.api.extensions.register_series_accessor("Drawdown")
 class Drawdown:
     """Given a Wealth Index DataFrame, will produce usefull drawdown metrics"""
 
-    def __init__(self, wealth_index_series: pd.Series):
-        self.wealth_index_series = wealth_index_series
-        peaks = self.wealth_index_series.cummax()
-        self.drawdown_series = (self.wealth_index_series - peaks) / peaks
-        self.drawdown_series.rename('Drawdown', inplace=True)
+    def __init__(self, pandas_obj):
+        self._validate(pandas_obj)
+        peaks = pandas_obj.cummax()
+        self._obj = (pandas_obj - peaks) / peaks
+        self._obj.rename('Drawdown', inplace=True)
 
-    def get_drawdown(self):
+    @staticmethod
+    def _validate(obj):
+        assert not pd.isnull(obj[0])
+        assert isinstance(obj.index, pd.DatetimeIndex)
+
+    @property
+    def drawdown(self):
         """
         Returns the pandas Series with Drawdown calculated.
 
@@ -24,36 +31,38 @@ class Drawdown:
             The Drawdown series
         """
 
-        return self.drawdown_series
+        return self._obj
 
-    def get_max_drawdown(self):
+    @property
+    def max_drawdown(self):
         """Returns the maximum drawdown"""
-        return self.drawdown_series.min()
+        return self._obj.min()
 
-    def compute_drawdown_lagoons_durations(self):
+    @property
+    def durations(self):
         """
         Returns a pd.Series where the index is the last day of the recovery of a lagoon
         and the value the total days since the previous max value for this lagoon
         """
         # find all the locations where the drawdown == 0
         zero_locations = np.unique(
-            np.r_[(self.drawdown_series == 0).values.nonzero()[0], len(self.drawdown_series) - 1]
+            np.r_[(self._obj == 0).values.nonzero()[0], len(self._obj) - 1]
         )
         # also assign the dates so we know when things were not sinking
-        zero_loc_s = pd.Series(zero_locations, index=self.drawdown_series.index[zero_locations])
+        zero_loc_s = pd.Series(zero_locations, index=self._obj.index[zero_locations])
         # do a shift to show what is the last and previous non zero dates
         df = zero_loc_s.to_frame('zero_loc')
         df['prev_zloc'] = zero_loc_s.shift()
         # keep only the dates where the difference is more than 1
         # that denotes the lagoons
         df = df[df['zero_loc'] - df['prev_zloc'] > 1].astype(int)
-        item = self.drawdown_series.index.__getitem__
+        item = self._obj.index.__getitem__
         df['Durations'] = df['zero_loc'].map(item) - df['prev_zloc'].map(item)
-        df = df.reindex(self.drawdown_series.index)
+        df = df.reindex(self._obj.index)
         df = df.dropna()
         return df['Durations']
 
-    def plot_drawdown(self, ax=None, **kwargs):
+    def plot(self, ax=None, **kwargs):
         """
         Plots the Drawdown
 
@@ -69,7 +78,7 @@ class Drawdown:
         if ax is None:
             ax = plt.gca()
 
-        series_to_plot = self.drawdown_series * 100
+        series_to_plot = self._obj * 100
         series_to_plot.plot(ax=ax, lw=2, kind='area', color='red', alpha=0.3, **kwargs)
         ax.yaxis.grid(linestyle=':')
         ax.xaxis.grid(linestyle=':')
--- pyinvestingsnippets/wealthindex.py
+++ pyinvestingsnippets/wealthindex.py
@@ -1,86 +1,59 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
+# import matplotlib.ticker as mtick
 
 
+@pd.api.extensions.register_series_accessor("WealthIndex")
 class WealthIndex:
     """Given a Returns Series, will produce the Wealth Index on 1 unit, and other helper stats"""
 
-    def __init__(self, returns_series: pd.Series):
-        # assert not returns_series.isnull().values.any()
-        assert pd.isnull(returns_series[0])
-        assert isinstance(returns_series.index, pd.DatetimeIndex)
-        self.returns_series = returns_series
-        self.wealth_index = ((returns_series + 1).cumprod()) * 1
-        self.wealth_index.rename('WealthIndex', inplace=True)
-        # We do not calculate these in here to avoid unnessessary computations
-        self.total_return = None
-        self.cagr = None
-        self.monthly_returns = None
-        self.positive_monthly_pct = None
-        self.annual_returns = None
+    def __init__(self, pandas_obj):
+        self._validate(pandas_obj)
+        self._obj = ((pandas_obj + 1).cumprod()) * 1
+        self._obj.rename('WealthIndex', inplace=True)
 
-    def get_wealth_index(self):
-        """
-        Returns the pandas Series with Wealth Index calculated.
+    @staticmethod
+    def _validate(obj):
+        assert pd.isnull(obj[0])
+        assert isinstance(obj.index, pd.DatetimeIndex)
 
-        Returns
-        -------
-        pd.Series
-            The Wealth Index series
-        """
+    @property
+    def wealth_index(self):
+        return self._obj
 
-        return self.wealth_index
-
-    def get_total_return(self):
+    @property
+    def total_return(self):
         """Returns the total return"""
-        if self.total_return:
-            return self.total_return
+        return self._obj.iloc[-1]
 
-        self.total_return = self.wealth_index.iloc[-1]
-        return self.total_return
-
-    def get_compound_annual_growth_rate(self):
+    @property
+    def cagr(self):
         """
         Returns compound_annual_growth_rate
         """
-        if self.cagr:
-            return self.cagr
+        no_of_years = len(np.unique(self._obj.index.year))
+        return (self._obj.iloc[-1] ** (1 / no_of_years)) - 1
 
-        no_of_years = len(np.unique(self.wealth_index.index.year))
-        self.cagr = (self.wealth_index.iloc[-1] ** (1 / no_of_years)) - 1
-        return self.cagr
+    @property
+    def monthly_rets(self):
+        return self._obj.resample('BM').apply(lambda x: x[-1]).pct_change()
 
-    def get_monthly_returns(self):
-        if self.monthly_returns:
-            return self.get_monthly_returns
-
-        self.monthly_returns = self.wealth_index.resample('BM').apply(lambda x: x[-1]).pct_change()
-        return self.monthly_returns
-
-    def return_positive_monthly_returns_percentage(self):
-        if self.positive_monthly_pct:
-            return self.positive_monthly_pct
-
-        monthly_rets = self.get_monthly_returns()
-        self.positive_monthly_pct = round(
-            monthly_rets[monthly_rets > 0].shape[0] / monthly_rets.shape[0] * 100, 2
+    @property
+    def positive_monthly_returns_percentage(self):
+        return round(
+            self.monthly_rets[self.monthly_rets > 0].shape[0] / self.monthly_rets.shape[0] * 100, 2
         )
-        return self.positive_monthly_pct
 
+    @property
     def get_annual_returns(self):
-        if self.annual_returns:
-            return self.annual_returns
+        return self._obj.resample('Y').apply(lambda x: x[-1]).pct_change()
 
-        self.annual_returns = self.wealth_index.resample('Y').apply(lambda x: x[-1]).pct_change()
-        return self.annual_returns
-
-    def plot_wealth_index(self, ax=None, **kwargs):
+    def plot(self, ax=None, **kwargs):
         if ax is None:
             ax = plt.gca()
 
-        self.wealth_index.plot(lw=2, color='green', alpha=0.7, x_compat=False, ax=ax, **kwargs)
+        self._obj.plot(lw=2, color='green', alpha=0.7, x_compat=False, ax=ax, **kwargs)
         ax.yaxis.grid(linestyle=':')
         ax.xaxis.grid(linestyle=':')
         ax.set_ylabel('')
@@ -92,42 +65,42 @@ class WealthIndex:
         ax.set_ylabel('Wealth Index')
         return ax
 
-    def plot_yearly_returns(self, ax=None, **kwargs):
-        if ax is None:
-            ax = plt.gca()
+    # def plot_yearly_returns(self, ax=None, **kwargs):
+    #     if ax is None:
+    #         ax = plt.gca()
 
-        self.get_annual_returns().plot(ax=ax, kind="bar")
-        ax.yaxis.grid(linestyle=':')
-        ax.xaxis.grid(linestyle=':')
-        ax.set_ylabel('')
-        ax.set_xlabel('')
-        ax.xaxis.grid(False)
-        plt.setp(ax.get_xticklabels(), visible=True, rotation=0, ha='center')
+    #     self.get_annual_returns().plot(ax=ax, kind="bar")
+    #     ax.yaxis.grid(linestyle=':')
+    #     ax.xaxis.grid(linestyle=':')
+    #     ax.set_ylabel('')
+    #     ax.set_xlabel('')
+    #     ax.xaxis.grid(False)
+    #     plt.setp(ax.get_xticklabels(), visible=True, rotation=0, ha='center')
 
-        ax.yaxis.set_major_formatter(mtick.PercentFormatter())
-        yearly_dates = [i for i in self.get_annual_returns().index.strftime('%Y')]
-        ax.set_xticklabels(yearly_dates, fontsize='small')
-        ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
+    #     ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    #     yearly_dates = [i for i in self.get_annual_returns().index.strftime('%Y')]
+    #     ax.set_xticklabels(yearly_dates, fontsize='small')
+    #     ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
 
-        ax.set_title('Yearly Returns (%)', fontweight='bold')
-        return ax
+    #     ax.set_title('Yearly Returns (%)', fontweight='bold')
+    #     return ax
 
-    def plot_monthly_returns(self, ax=None, **kwargs):
-        if ax is None:
-            ax = plt.gca()
+    # def plot_monthly_returns(self, ax=None, **kwargs):
+    #     if ax is None:
+    #         ax = plt.gca()
 
-        self.get_monthly_returns().plot(ax=ax, kind="bar")
-        ax.yaxis.grid(linestyle=':')
-        ax.xaxis.grid(linestyle=':')
-        ax.set_ylabel('')
-        ax.set_xlabel('')
-        ax.xaxis.grid(False)
-        plt.setp(ax.get_xticklabels(), visible=True, rotation=0, ha='center')
+    #     self.get_monthly_returns().plot(ax=ax, kind="bar")
+    #     ax.yaxis.grid(linestyle=':')
+    #     ax.xaxis.grid(linestyle=':')
+    #     ax.set_ylabel('')
+    #     ax.set_xlabel('')
+    #     ax.xaxis.grid(False)
+    #     plt.setp(ax.get_xticklabels(), visible=True, rotation=0, ha='center')
 
-        ax.yaxis.set_major_formatter(mtick.PercentFormatter())
-        monthly_dates = [i for i in self.get_monthly_returns().index.strftime('%Y-%m')]
-        ax.set_xticklabels(monthly_dates, fontsize='small')
-        ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
+    #     ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    #     monthly_dates = [i for i in self.get_monthly_returns().index.strftime('%Y-%m')]
+    #     ax.set_xticklabels(monthly_dates, fontsize='small')
+    #     ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
 
-        ax.set_title('Monthly Returns (%)', fontweight='bold')
-        return ax
+    #     ax.set_title('Monthly Returns (%)', fontweight='bold')
+    #     return ax
