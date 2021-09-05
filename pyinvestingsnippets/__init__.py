2511655f23087c4ac4b91af162d4804bba061d9e
investingsnippets <investingsnippets@gmail.com>
2021-09-05 16:23:39 +0200

    Fixes #7: Adds tests and exposes wealth index

--- pyinvestingsnippets/__init__.py
+++ pyinvestingsnippets/__init__.py
@@ -1,6 +1,7 @@
 """Python tools for stock analysis"""
 from .drawdown import Drawdown
+from .wealthindex import WealthIndex
 
 
 __version__ = '0.0.5'
-__all__ = ['Drawdown']
+__all__ = ['Drawdown', 'WealthIndex']
