90f6fc8a3cc092e8d7d15c366a57ea95f78e2ec2
investingsnippets <investingsnippets@gmail.com>
2022-01-17 15:45:17 +0100

    v1.3.2

--- pyinvestingsnippets/__init__.py
+++ pyinvestingsnippets/__init__.py
@@ -21,7 +21,7 @@ from .utilities.beta_covariance import BetaCovariance
 from .utilities.beta_regression import BetaRegression
 
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 __all__ = [
     "Drawdown",
     "WealthIndex",
