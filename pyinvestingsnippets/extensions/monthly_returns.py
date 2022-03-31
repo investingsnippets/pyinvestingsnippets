e8b3cd9dc32dcba131be4a9ae6231314df3c35d3
investingsnippets <investingsnippets@gmail.com>
2022-03-31 22:00:00 +0200

    lint

--- pyinvestingsnippets/extensions/monthly_returns.py
+++ pyinvestingsnippets/extensions/monthly_returns.py
@@ -15,9 +15,7 @@ class MonthlyReturns:
 
     def __init__(self, pandas_obj):
         self._validate(pandas_obj)
-        self._obj = (
-            pandas_obj.fillna(method="pad").resample("M").last().pct_change()
-        )
+        self._obj = pandas_obj.fillna(method="pad").resample("M").last().pct_change()
 
     @staticmethod
     def _validate(obj):
